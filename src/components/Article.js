23278d28f74bb9cc24bef399c4a15e4cbd99cd0e
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-05-24 15:54:02 -0700

    strech goal last task remaining

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 function Article(props) {
-  const { title, date, preview } = props;
+  const { title, date, preview, minutes } = props;
 
   return (
     <article>
       <h3>{title}</h3>
       <small>{date || "January 1, 1970"}</small>
+      <br></br>
+      <small>{`Minutes to read: ${minutes}`}</small>
       <p>{preview}</p>
     </article>
   );
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,11 +4,12 @@ import Article from "./Article";
 
 function ArticleList(props) {
   const { posts } = props;
+  console.log(props);
 
   return (
     <main>
       {posts.map((post) => (
-        <Article key={uuidv4()} title={post.title} date={post.date} preview={post.preview} />
+        <Article key={uuidv4()} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes} />
       ))}
     </main>
   );
